30da3ea429ab0a546e00cc7f7e5e966cbd06b2a7
Vaishnavi Patil <vaishkpatil1217@gmail.com>
2025-12-11 18:23:27 +0530

    pattern

---
+++ pattern11.py
@@ -0,0 +1,21 @@
+for i in range(1,6):
+    for j in range(6-i):
+     print(" ",end="")
+    for m in range(i):
+        print("* ",end="")
+    print("")
+    
+for i in range(0,2):
+    for j in range(0,1):
+        print("     *",end=" ")
+    print()
+    
+    """
+     * 
+    * *
+   * * *
+  * * * *
+ * * * * *
+     *
+     *
+    """
