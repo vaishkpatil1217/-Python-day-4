e6fb303a75d53a0a9d041a80fd0564a7578e94c4
Vaishnavi Patil <vaishkpatil1217@gmail.com>
2025-12-11 18:18:30 +0530

    pattern

---
+++ pattern7.py
@@ -0,0 +1,13 @@
+for i in range(1,6):
+    for j in range(6-i):
+        print(" ",end="")
+    for k in range(i):
+         print("*",end="")
+    print("")
+    """
+     *
+    **
+   ***
+  ****
+ *****
+    """
